caaf07e51e0856464761e0ccca528c107f746112
GuYuanjie <1982301947@qq.com>
2025-12-24 14:22:34 +0800

    about me

--- google_scholar_crawler/main.py
+++ google_scholar_crawler/main.py
@@ -3,7 +3,11 @@ import jsonpickle
 import json
 from datetime import datetime
 import os
+from scholarly import scholarly, ProxyGenerator
 
+pg = ProxyGenerator()
+pg.FreeProxies()
+scholarly.use_proxy(pg)
 author: dict = scholarly.search_author_id(os.environ['GOOGLE_SCHOLAR_ID'])
 scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
 name = author['name']
